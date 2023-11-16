94224df4b548eca4f91ab4c6c9596cf9d90dc4bf
emilykamiti <emilykamiti@gmail.com>
2023-11-16 05:18:28 +0300

    Annotations
    Continued applying annotations on my files

--- src/main/java/com/resale/action/HomeAction.java
+++ src/main/java/com/resale/action/HomeAction.java
@@ -1,15 +1,17 @@
 package com.resale.action;
 
 import com.resale.app.view.html.AppPage;
+import com.resale.app.view.html.HtmlTable;
 import com.resale.app.bean.ItemBean;
 import com.resale.app.bean.ItemBeanI;
 import com.resale.app.model.entity.Item;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebServlet("/home")
@@ -19,9 +21,11 @@ public class HomeAction extends BaseAction {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String addItemLink = "<br/><a href=\"" + req.getContextPath() + "/add-item.jsp\">Add Item</a>";
-        String itemListing = itemBean.showItems();
-        new AppPage().renderHtml(req, resp, 0, addItemLink + itemListing);
+        HttpSession httpSession = req.getSession();
+
+        new AppPage().renderHtml(req, resp, 0,
+                HtmlTable.form(Item.class) + itemBean.showItems());
+
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
